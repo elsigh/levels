09a4bab50100bf6f385e925d8fb22c05491ef399
Lindsey Simon <elsigh@gmail.com>
2013-05-06 23:36:53 -0700

    oauth with session token

--- server/lib/www.py
+++ server/lib/www.py
@@ -2,6 +2,8 @@
 #
 #
 
+import datetime
+import json
 import logging
 import os
 import sys
@@ -25,19 +27,27 @@ class ProfileHandler(WebRequestHandler):
   def get(self, user_key=None):
     """Handles GET /profile"""
 
+    close = self.request.get('close')
+
     if self.logged_in and user_key is None:
       self.output_response({
-        'user': self.current_user,
-        'session': self.auth.get_user_by_session()
+          'user': self.current_user,
+          'session': self.auth.get_user_by_session(),
+          'close': close,
       }, 'profile.html')
 
     elif user_key is not None:
-        user = ndb.Key(urlsafe=user_key).get()
+        try:
+            user = ndb.Key(urlsafe=user_key).get()
+        except:
+            user = User()
         logging.info('user: %s', user.to_dict())
 
         template_data = user.get_template_data()
+
         template_data.update({
-            'session': self.auth.get_user_by_session()
+            'session': self.auth.get_user_by_session(),
+            'close': close,
         })
         #template_data['title'] = template_data['profile']['username'] + \
         #    ' ' + str(template_data['devices'][0]['last_settings']['battery_level']) + \
